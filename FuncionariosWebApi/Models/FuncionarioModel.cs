86a92003b6e8c449fbc34bdd4bcc77cf8539e104
Samuel Filgueira <leumas685@gmail.com>
2023-12-21 07:03:08 -0300

    Criação das models e enumeradores

---
+++ FuncionariosWebApi/Models/FuncionarioModel.cs
@@ -0,0 +1,18 @@
+﻿using FuncionariosWebApi.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FuncionariosWebApi.Models
+{
+    public class FuncionarioModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
+        public DepartamentoEnum Departanemto { get; set; }
+        public TurnoEnum Turno { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
+    }
+}
