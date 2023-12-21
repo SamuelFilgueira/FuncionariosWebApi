2582cfdcdbf80d70dcf7cf97c7c88abbe0e9439e
Samuel Filgueira <leumas685@gmail.com>
2023-12-21 07:15:43 -0300

    Criação do dataContext

---
+++ FuncionariosWebApi/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FuncionariosWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuncionariosWebApi.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        DbSet<FuncionarioModel> Funcionarios {  get; set; }
+    }
+}
